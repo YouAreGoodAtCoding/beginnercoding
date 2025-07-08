a5b41698c2ae9c855baa99da8562a9e0b810b680
VidhyaPG <pg.vidhyap@gmail.com>
2025-07-07 17:58:04 -0700

    Day 4

--- python/testscores.py
+++ python/testscores.py
@@ -6,7 +6,28 @@ def count_students_above(scores, threshold):
     return count
 
 
+def print_total_and_average(scores):
+    sum = 0
+    for score in scores:
+        sum += score
+    print(f"Sum of all scores is {sum}")
+    count = len(scores)
+    average = sum / count
+    print(f"The average score is: {average}")
+
+
+def print_highest_score(scores):
+    # works for both int and float
+    max = float('-inf')
+    for score in scores:
+        if (score > max):
+            max = score
+    print(f"The highest score is: {max}")
+
+
 if __name__ == "__main__":
     scores = [20, 44, 7, 89, 80, 94, 79]
     threshold = 80
     print(f"{count_students_above(scores, threshold)} students scored above {threshold}")
+    print_total_and_average(scores)
+    print_highest_score(scores)
